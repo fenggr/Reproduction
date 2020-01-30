8b0f4b4f4f07a73bfa074eba43092446bc18c774
fenggr <fenggr@gmail.com>
2020-01-30 22:55:54 +0800

    修改了main.py

--- main.py
+++ main.py
@@ -3,10 +3,13 @@
 import human as hm
 
 w = hm.Woman()
+m = hm.Man()
 
 print("-------Reproduction-------")
 print(w.age, w.hy)
+m.add_age()
 w.add_age()
 w.set_hy()
+print("男人的年龄：{}岁".format(m.age))
 print(w.age, w.hy)
 
